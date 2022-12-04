8b2f975ebecd93208244212e53f512aec27d8197
Mike Repec <mikerepec@gmail.com>
2022-12-04 19:36:22 +0100

    Init Day 4

---
+++ day_4.ts
@@ -0,0 +1,25 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+
+import { readInput } from "./util.ts";
+
+const day = 4;
+const theme = "Camp Cleanup";
+
+console.log(`Day ${day}: ${theme}\n`);
+
+function part1(_input: string): number {
+  return 0;
+}
+
+function part2(_input: string): number {
+  return 0;
+}
+
+const input = readInput(`day_${day}_input`);
+const testInput = readInput(`day_${day}_test_input`);
+
+assertEquals(part1(testInput), 0);
+assertEquals(part2(testInput), 0);
+
+console.log("Part 1:", part1(input));
+console.log("Part 2:", part2(input));
