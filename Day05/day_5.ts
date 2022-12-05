e593173a7fe923366535adc3ab4ba31c9cdea89b
Mike Repec <mikerepec@gmail.com>
2022-12-05 23:21:19 +0100

    Day 5: Part 2

--- Day05/day_5.ts
+++ Day05/day_5.ts
@@ -11,6 +11,24 @@ function part1(input: string): string {
   const [stacksInput, instructionsInput] = input.split("\n\n")
     .map((part) => part.split("\n"));
 
+  return performInstructions(
+    buildStacks(stacksInput),
+    instructionsInput,
+  ).map((stack) => stack[0] ?? "").join("");
+}
+
+function part2(input: string): string {
+  const [stacksInput, instructionsInput] = input.split("\n\n")
+    .map((part) => part.split("\n"));
+
+  return performInstructions(
+    buildStacks(stacksInput),
+    instructionsInput,
+    false,
+  ).map((stack) => stack[0] ?? "").join("");
+}
+
+function buildStacks(stacksInput: string[]): string[][] {
   const stackIndexes = [
     ...(stacksInput.splice(-1)[0].matchAll(/\d+/g)),
   ].map((m) => m.index as number);
@@ -22,26 +40,34 @@ function part1(input: string): string {
       .forEach((item, i) => item.trim() && stacks[i].push(item))
   );
 
+  return stacks;
+}
+
+function performInstructions(
+  stacks: string[][],
+  instructionsInput: string[],
+  reverseStackMove = true,
+): string[][] {
+  const stacksCopy = [...(stacks.map((stack) => [...stack]))];
+
   instructionsInput
     .map((line) => line.split(" ").map((el) => parseInt(el)).filter(Boolean))
     .map(([amount, src, dest]) => [amount, src - 1, dest - 1])
     .forEach(([amount, src, dest]) => {
-      const itemsToMove = stacks[src].splice(0, amount).reverse();
-      stacks[dest] = [...itemsToMove, ...stacks[dest]];
+      const movedItems = stacksCopy[src].splice(0, amount);
+      if (reverseStackMove) movedItems.reverse();
+
+      stacksCopy[dest] = [...movedItems, ...stacksCopy[dest]];
     });
 
-  return stacks.map((stack) => stack[0] ?? "").join('');
-}
-
-function part2(_input: string): string {
-  return "";
+  return stacksCopy;
 }
 
 const input = readInput(`day_${day}_input`);
 const testInput = readInput(`day_${day}_test_input`);
 
 assertEquals(part1(testInput), "CMZ");
-assertEquals(part2(testInput), "");
+assertEquals(part2(testInput), "MCD");
 
 console.log("Part 1:", part1(input));
 console.log("Part 2:", part2(input));
