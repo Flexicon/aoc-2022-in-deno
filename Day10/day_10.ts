f5982b5f1bcf65de844ba6690e45c81f5644956a
Mike Repec <mikerepec@gmail.com>
2022-12-10 16:41:14 +0100

    Day 10: Part 2

--- Day10/day_10.ts
+++ Day10/day_10.ts
@@ -3,14 +3,41 @@ import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
 import { readInput } from "../util.ts";
 
 const day = 10;
-const theme = "TBD";
+const theme = "Cathode-Ray Tube";
 
 console.log(`Day ${day}: ${theme}\n`);
 
 function part1(input: string): number {
   const instructions = input.split("\n");
+  const cycles: number[] = buildCycles(instructions);
 
-  const cycles: number[] = instructions
+  return measureSignalStrength(cycles);
+}
+
+function part2(input: string): string {
+  const instructions = input.split("\n");
+  const cycles: number[] = buildCycles(instructions);
+
+  // Draw to screen buffer
+  let buffer = "";
+  for (let i = 0; i < cycles.length; i++) {
+    const spriteIndex = cycles[i] - 1;
+    const pos = i % 40;
+
+    if (pos < spriteIndex || pos > (spriteIndex + 2)) {
+      buffer += ".";
+    } else {
+      buffer += "#";
+    }
+
+    if (pos === 39 && i < cycles.length - 1) buffer += "\n";
+  }
+
+  return buffer;
+}
+
+function buildCycles(instructions: string[]): number[] {
+  const cycles = instructions
     .map((line) => line.split(" "))
     .reduce((cycles, [instruction, arg]) => {
       const register = cycles.slice(-1)[0];
@@ -29,11 +56,7 @@ function part1(input: string): number {
       return cycles;
     }, [1]);
 
-  return measureSignalStrength(cycles);
-}
-
-function part2(_input: string): number {
-  return 0;
+  return cycles.slice(0, -(cycles.length % 40));
 }
 
 function measureSignalStrength(cycles: number[]): number {
@@ -48,9 +71,10 @@ function measureSignalStrength(cycles: number[]): number {
 
 const input = readInput(`day_${day}_input`);
 const testInput = readInput(`day_${day}_test_input`);
+const test2Output = readInput(`day_${day}_test_2_output`);
 
 assertEquals(part1(testInput), 13140);
-assertEquals(part2(testInput), 0);
+assertEquals(part2(testInput), test2Output);
 
 console.log("Part 1:", part1(input));
-console.log("Part 2:", part2(input));
+console.log(`Part 2: 👇\n${part2(input)}`);
